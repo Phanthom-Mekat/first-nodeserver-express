4db458e097893dda7f76b2173ee949e31f22b242
Phanthom-Mekat <jahidulhossainmekat@gmail.com>
2024-11-23 12:58:19 +0600

    phone api created

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express')
+const phones = require('./phones.json');
 const app = express()
 const port =5000;
 
 app.get('/',(req,res)=>{
     res.send('hello first express')
+})
+
+app.get('/phones',(req,res)=>{
+    res.send(phones)
+})
+app.get('/phones/:id',(req,res)=>{
+    const id = parseInt(req.params.id)
+    console.log('I need data for id',id)
+    const phone = phones.find(phone=>phone.id===id)|| {};
+    res.send(phone)
 })
 
 app.get('/about',(req,res)=>{
@@ -13,3 +24,4 @@ app.get('/about',(req,res)=>{
 app.listen(port,()=>{
     console.log(`Hello running port,${port} `)
 })
+
